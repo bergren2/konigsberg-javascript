f9e5157fa7e9c07327a01f82efbd6451462a3336
Dan Bergren <bergren2@users.noreply.github.com>
2021-12-05 14:32:04 -0600

    more setup

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # [Königsberg](https://github.com/bergren2/konigsberg) - JavaScript
 
+## Prereqs
+
+- [fast node manager](https://github.com/Schniz/fnm)
+
+## Setup
+
 ```shell
 npm install
 npm test
---
+++ test/Year2021Day1Tests.js
@@ -0,0 +1,9 @@
+import assert from 'assert';
+
+describe("Year 2021 Day 1", function () {
+    describe("Part 1", function () {
+        it("should solve the simple case", function () {
+            assert.equal(7, 7);
+        });
+    });
+});
